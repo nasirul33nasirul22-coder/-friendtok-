36f87a9aa93af3a4ef2e5896821434591a22fdf7
Nasirul Islam Moon <nasirul33nasirul22@gmail.com>
2026-07-22 13:26:34 +0600

    sync capacitor android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.friendtok.app',
+  appName: 'FriendTok',
+  webDir: 'dist'
+};
+
+export default config;
